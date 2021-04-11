222319465d350a27a530dbcb2af6ad9b77bcc42a
Yusufu Kamara <yu_kamara@yahoo.com>
2021-04-11 15:13:54 -0700

    Updated the code for the PyBank challenge

--- 02-python-challenge/PyBank/main.py
+++ 02-python-challenge/PyBank/main.py
@@ -2,32 +2,67 @@
 import os
 import csv
 
-# set the path for collecting the ellection data
-budget_csv = os.path.join('Resources', 'budget_data.csv')
+def main():
+    # set the path for collecting the ellection data
+    # Initialize variables
+    csv_path = os.path.join('Resources', 'budget_data.csv')
+    csv_write = os.path.join('Analysis', 'Budget_Analysis.txt')
+    total_months = 0
+    net_total_amount = 0
+    average_change = 0
+    greatest_increase = float('-inf')   # sets the lower limit for the revenue change
+    greatest_decrease = float('inf')    # sets the upper limit for the revenue change
+    greatest_increase_date = 'Mon-Year'
+    greatest_decrease_date = 'Mon-Year'
+    prev_revenue = None     # this enebles us to skip the first revenue row when computing the revenue change
+    revenue_change_ls = []
 
-with open(budget_csv, newline='', encoding='utf-8') as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter=',')
+    # read the csv
+    with open(csv_path, newline='') as f: #quivalent to f = open(csv_reader, 'r'), f('close')
+        reader = csv.reader(f, delimiter=',')
+        #skip header
+        csv_header = next(f, None)
+        # loop thru data
+        for row in reader:
+            revenue = int(row[1]) 
+            # update total months, this gives us the total number of months
+            total_months += 1
+            # update the net total revenue
+            net_total_amount += revenue
+            # this next code will skip the first revenue row since there is no previous revenue to compute a change
+            if prev_revenue is not None:
+                # compute the change in revenue
+                rev_change = revenue - prev_revenue
+                # append to the revenue change list
+                revenue_change_ls.append(rev_change)
+                # update the greatest increase
+                if rev_change > greatest_increase:
+                    greatest_increase = rev_change
+                    greatest_increase_date = row[0]
+                # update the greatest decrease
+                if rev_change < greatest_decrease:
+                    greatest_decrease = rev_change
+                    greatest_decrease_date = row[0]
+            # update prev_revenue
+            prev_revenue = revenue
+    #update average change
+    average_change += round(sum(revenue_change_ls) / len(revenue_change_ls), 2)
 
-    for row in csv_reader:
-        print(row)
+    output = (
+        f'Financial Analysis\n'
+        f'-------------------------\n'
+        f'Total Months: {total_months}\n'
+        f'Total: ${net_total_amount}\n'
+        f'Average Change: ${average_change}\n'
+        f'Greatest increase in profits: {greatest_increase_date} (${greatest_increase})\n'
+        f'Greatest decrease in profits: {greatest_decrease_date} (${greatest_decrease})\n'
+    )
 
-# TASKS
-# 1. The total number of months included in the dataset
-    for line in csv_reader:
-        no_of_months = sum(1 for row in csv_reader)
-        print(no_of_months)
+    print (output)
 
+    with open(csv_write, 'w') as f:
+        f.write(output)
 
-# 2. The net total amount of "Profit/Losses" over the entire period
-    #total_profit_loss = 0
-    #for x in 
-    profit_loss = ([sum(x) for x in zip(*csv_reader)])
-    print(profit_loss)
-
-# 3. Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-
-
-# 4. The greatest increase in profits (date and amount) over the entire period
-
-
-# 5. The greatest decrease in losses (date and amount) over the entire period
+if __name__ == "__main__":
+    main()
+        
